1dc426188ae29c397b69fb5844e1761c8a87861a
Alexandre <alexandre.venturi@outlook.com>
2025-11-13 14:27:36 +0100

    i

--- data_cleaner.py
+++ data_cleaner.py
@@ -2,6 +2,7 @@ import pickle
 import lzma
 import os
 from PIL import Image
+import tqdm
 
 def all_controls_zero(snap):
     return all(v == 0 for v in snap.current_controls)
@@ -17,6 +18,10 @@ def data_cleaner(path):
     end = len(data) - 1
     while end >= 0 and all_controls_zero(data[end]):
         end -= 1
+
+    if start == 0 and end == len(data) - 1:
+        print(f"Already clean → skipped: {path}")
+        return
 
     data = data[start:end+1] if start <= end else []
 
